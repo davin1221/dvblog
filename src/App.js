34c58eef3542818d2a3e2a1ca80dc3d8d0ffd59a
Davin <kdavin1221@gmail.com>
2023-08-09 23:38:30 +0900

    블로그 실습

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = "강남 우동 맛집!!"
+  // 자료를 잠깐 보관 = state 
+  const [글제목, setData] = useState('남자 코트 추천');
+
   return (
     <div className="App">
       <div className='black-nav'>
         <h4 style={{ fontSize: "30px" }}>DV BLOG</h4>
       </div>
 
-      <h3>{ post }</h3>
+      <div className='list'>
+        <h3>{글제목}</h3>
+        <p>2023-08-09</p>
+      </div>
+
     </div>
   );
 }
